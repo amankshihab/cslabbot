43d85b0df68d8bd11f1a72bf9ec12f24fcdd7163
Aman K Shihab <amanshihab276@gmail.com>
2021-01-12 19:27:35 +0530

    Added smaple response

--- bot.py
+++ bot.py
@@ -3,3 +3,7 @@ import os
 from secrets import TOKEN
 
 bot = telebot.TeleBot(TOKEN, parse_mode = None)
+
+@bot.message_handler(commands=['start', 'help'])
+def print_message(message):
+    bot.reply_to(message, "Hey there!")
