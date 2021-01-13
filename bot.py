c5234105f99cb2be7d49e904a2481924b41feb1d
Aman K Shihab <amanshihab276@gmail.com>
2021-01-13 19:14:47 +0530

    Adding tt

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from secrets import TOKEN
+from resources.secrets import TOKEN
+from resources.tt import tt
+from datetime import datetime
 
 #intro = 'Hey there! I am the CSLAbot. I am WIP bot. So wait for further features'
 
@@ -14,11 +16,16 @@ def intro(update, context):
 	#context.bot.reply_text(chat_id=update.effective_chat.id, text='Hey there! I am the CSLAbot. I am WIP bot. So wait for further features')
     update.message.reply_text('Hey there! I am the CSLAbot. I am WIP bot. So wait for further features')
 
+def timetable(update, context):
+    update.message.reply_text(tt["Monday"])
+
 hello_handler = CommandHandler('hello', hello)
 intro_handler = CommandHandler('who', intro)
+tt_handler = CommandHandler('tt', timetable)
 
 dispatcher.add_handler(hello_handler)
 dispatcher.add_handler(intro_handler)
+dispatcher.add_handler(tt_handler)
 
 
 updater.start_polling()
---
+++ resources/tt.py
@@ -0,0 +1,111 @@
+#from datetime import datetime
+
+"""tt = {
+    "Monday":{
+                1:{
+                    "Subject" : "Java",
+                    "Start" : "9:00",
+                    "End" : "10:00"
+                },
+                2:{
+                    "Subject" : "Maths",
+                    "Start" : "10:30",
+                    "End" : "11:30"
+                },
+                3:{
+                    "Subject" : "Sus. Engg.",
+                    "Start" : "12:00",
+                    "End" : "13:00"
+                },
+                4:{
+                    "Subject" : "Lab",
+                    "Start" : "14:00",
+                    "End" : "16:00"
+                }    
+            },
+    "Tuesday":{
+                1:{
+                    "Subject" : "Java",
+                    "Start" : "9:00",
+                    "End" : "10:00"
+                },
+                2:{
+                    "Subject" : "Maths",
+                    "Start" : "10:30",
+                    "End" : "11:30"
+                },
+                3:{
+                    "Subject" : "Sus. Engg.",
+                    "Start" : "12:00",
+                    "End" : "13:00"
+                }
+            },
+    "Wednesday":{
+                1:{
+                    "Subject" : "Java",
+                    "Start" : "9:00",
+                    "End" : "10:00"
+                },
+                2:{
+                    "Subject" : "Maths",
+                    "Start" : "10:30",
+                    "End" : "11:30"
+                },
+                3:{
+                    "Subject" : "Sus. Engg.",
+                    "Start" : "12:00",
+                    "End" : "13:00"
+                },
+                4:{
+                    "Subject" : "Lab",
+                    "Start" : "14:00",
+                    "End" : "16:00"
+                }
+            }, 
+    "Thursday":{
+                1:{
+                    "Subject" : "Java",
+                    "Start" : "9:00",
+                    "End" : "10:00"
+                },
+                2:{
+                    "Subject" : "Maths",
+                    "Start" : "10:30",
+                    "End" : "11:30"
+                },
+                3:{
+                    "Subject" : "Sus. Engg.",
+                    "Start" : "12:00",
+                    "End" : "13:00"
+                }
+            },
+    "Friday":{
+                1:{
+                    "Subject" : "Java",
+                    "Start" : "9:00",
+                    "End" : "10:00"
+                },
+                2:{
+                    "Subject" : "Maths",
+                    "Start" : "10:30",
+                    "End" : "11:30"
+                },
+                3:{
+                    "Subject" : "Sus. Engg.",
+                    "Start" : "12:00",
+                    "End" : "13:00"
+                }
+            }
+}"""
+
+tt = {
+    "Monday":{
+                """Java (9 - 10)
+                Maths(10:30-11:30)
+                Sus. Engg. (12 - 1)
+                Lab(2-4)"""
+            }  
+}  
+
+#today = datetime.today().strftime("%A")
+print(tt["Monday"])
