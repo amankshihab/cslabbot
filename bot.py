a0cba519446296f4712e887dc9d74da1a9f43dca
Aman K Shihab <amanshihab276@gmail.com>
2021-01-13 21:19:52 +0530

    Modified response

--- bot.py
+++ bot.py
@@ -4,27 +4,27 @@ from resources.tt import tt
 from datetime import datetime, date
 import json
 
-#intro = 'Hey there! I am the CSLAbot. I am WIP bot. So wait for further features'
+intro = """Hey there! I am the CSLAbot. \nI am a WIP bot. \nSo stay tuned for more features\n send /help to know my features"""
 
 updater = Updater(token=TOKEN, use_context=True) #Replace TOKEN with your token string
 dispatcher = updater.dispatcher
 
 def hello(update, context):
     #context.bot.send_message(chat_id=update.effective_chat.id, text='Hello, World')
-    update.message.reply_text('Hello World')
+    user = update.message.from_user
+    hello = f"Hello {user['username']}!"
+    update.message.reply_text(hello)
 
 def intro(update, context):
 	#context.bot.reply_text(chat_id=update.effective_chat.id, text='Hey there! I am the CSLAbot. I am WIP bot. So wait for further features')
-    update.message.reply_text('Hey there! I am the CSLAbot. I am WIP bot. So wait for further features')
+    update.message.reply_text(intro)
 
 def timetable(update, context):
     today = datetime.today().strftime("%A")
 
-    text = f"**{today}({date.today()})**\n\n"
+    text = f"{today}({date.today()})\n\n"
     for period in tt[today]:
-        text += "__"
         text += tt[today][period]
-        text += "__"
         text += "\n"
 
     update.message.reply_text(text)
--- test.py
+++ test.py
@@ -1,15 +1,1 @@
-import json
-from datetime import datetime, date
-from resources.tt import tt
-
-
-today = datetime.today().strftime("%A")
-
-text = f"**{today}({date.today()})**\n\n"
-for period in tt[today]:
-    text += "__"
-    text += tt[today][period]
-    text += "__"
-    text += "\n"
-
-print(text)
+)
